8a17a8e88103bfc1b3cecbe057a0b809ebbb8e7c
Miguel Gargallo <mgargallo16@gmail.com>
2023-01-01 20:46:34 +0100

    1.0.0 Source Code

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 # Video Splitter
 
-This is a simple video splitter into multiple parts. It is based on the [FFmpeg](https://ffmpeg.org/) library.
+This is a simple video splitter into multiple parts of 12 minutes. It is based on the [FFmpeg](https://ffmpeg.org/) library.
--- index.js
+++ index.js
@@ -1,119 +1,110 @@
 const express = require('express')
-
 const ffmpeg = require('fluent-ffmpeg');
-
 const app = express()
-
-const zip= require('adm-zip')
-
-const multer= require('multer')
-
-const fs= require('fs')
-
-const path= require('path')
-
-const {exec}= require('child_process');
+const zip = require('adm-zip')
+const multer = require('multer')
+const fs = require('fs')
+const path = require('path')
+const { exec } = require('child_process');
 const admZip = require('adm-zip');
 
 app.use(express.static('public'))
 
-var dir= 'public'
+var dir = 'public'
+var subDir = 'public/uploads'
 
-var subDir= 'public/uploads'
-
-if(!fs.existsSync(dir)){
+if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir)
-
     fs.mkdirSync(subDir)
 }
 
-var storage= multer.diskStorage({
-    destination:function (req,file,cb) {
-        cb(null,'public/uploads')
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
     },
-    filename:function(req,file,cb){
-        cb(null,file.fieldname+ '-'+ Date.now()+ path.extname(file.originalname))
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
     }
 })
 
-var upload= multer({storage:storage})
+var upload = multer({ storage: storage })
 
-const PORT=  process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/home.html')
 })
 
-app.post('/convert',upload.single('file'),(req,res)=>{
-   if(req.file){
-       console.log(req.file.path)
+app.post('/convert', upload.single('file'), (req, res) => {
+    if (req.file) {
+        console.log(req.file.path)
 
-       const videoFile= req.file.path
+        const videoFile = req.file.path
 
-       ffmpeg.ffprobe(videoFile,(err,metaData)=>{
-     
-        const dur = metaData.format.duration;
-        const count= Math.ceil(dur/15)
-        const arr=[]
-        const fileNames=[]
-       
-        var sec=0;
-        var min=0;
-        var hour=0;
-        var duration=15;
-        for(var start=0;start<dur;start=start+15){
-            var output=Date.now()+'.mp4'
-            fileNames.push(output)
-           sec= parseInt(start%60);
-           min= parseInt(start/60);
-           hour= parseInt(hour/3600);
-          var startingTime=hour.toString()+":"+min.toString()+":"+sec.toString();
-           console.log(startingTime);
-           if(duration>dur-start){
-             duration= parseInt(dur-start);
-           }
-           console.log(duration);
-          ffmpeg()
-          .input(videoFile)
-          .setStartTime(startingTime)
-          .setDuration(duration.toString())
-          .output(output)
-          .on('end',()=> {
-              console.log("Done!")
-              arr.push('done')
-              if(arr.length===count){
-                var zip= new admZip()
-                for(let i=0;i<count;i++){
-                    zip.addLocalFile(fileNames[i])
+        ffmpeg.ffprobe(videoFile, (err, metaData) => {
+
+            const dur = metaData.format.duration;
+            const count = Math.ceil(dur / 720) // 720 seconds is 12 minutes
+            const arr = []
+            const fileNames = []
+
+            var sec = 0;
+            var min = 0;
+            var hour = 0;
+            var duration = 720;
+            for (var start = 0; start < dur; start = start + 720) {
+                var output = Date.now() + '.mp4'
+                fileNames.push(output)
+                sec = parseInt(start % 60);
+                min = parseInt(start / 60);
+                hour = parseInt(hour / 3600);
+                var startingTime = hour.toString() + ":" + min.toString() + ":" + sec.toString();
+                console.log(startingTime);
+                if (duration > dur - start) {
+                    duration = parseInt(dur - start);
                 }
-                var outputfile= "splitted.zip"
-                fs.writeFileSync(outputfile,zip.toBuffer());
-                res.download(outputfile,(err)=>{
-                    if(err) {
-                        console.log(err)
-                    }
-                    for(let i=0;i<count;i++){
-                        fs.unlinkSync(fileNames[i])
-                    }
-                    fs.unlinkSync(outputfile)
-                    fs.unlinkSync(videoFile)
-                })
-                
-              }
-            
-            
-            })
-          .on('error',(err)=> console.error(err))
-          .run();
-        }
-        
-    });
+                console.log(duration);
+                ffmpeg()
+                    .input(videoFile)
+                    .setStartTime(startingTime)
+                    .setDuration(duration.toString())
+                    .output(output)
+                    .on('end', () => {
+                        console.log("Done!")
+                        arr.push('done')
+                        if (arr.length === count) {
+                            var zip = new admZip()
+                            for (let i = 0; i < count; i++) {
+                                zip.addLocalFile(fileNames[i])
+                            }
+                            var outputfile = "splitted.zip"
+                            fs.writeFileSync(outputfile, zip.toBuffer());
+                            res.download(outputfile, (err) => {
+                                if (err) {
+                                    console.log(err)
+                                }
+                                for (let i = 0; i < count; i++) {
+                                    fs.unlinkSync(fileNames[i])
+                                }
+                                fs.unlinkSync(outputfile)
+                                fs.unlinkSync(videoFile)
+                            })
 
-    
-   }
-   console.log('done splitting')
+                        }
+
+
+                    })
+                    .on('error', (err) => console.error(err))
+                    .run();
+            }
+
+        });
+
+
+    }
+    console.log('done splitting')
 })
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`)
 })
